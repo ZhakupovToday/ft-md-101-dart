9cbe0dd02bf9e4674cfbb446920ead0e0b6db70d
ZhakupovToday <Zhakupov.work@mail.ru>
2022-07-10 20:36:17 +0300

    Урок №16 - Абстрактные классы

---
+++ lib/steps/utils/DartAbstractChild.dart
@@ -0,0 +1,8 @@
+import 'package:ft_md_101_dart/steps/utils/DartAbstractParent.dart';
+
+class DartAbstractChild extends DartAbstractParent {
+  @override
+  void PrintMe() {
+    super.printAbstract();
+  }
+}
