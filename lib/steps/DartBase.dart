9bf6ae4e14fe445b7d243fbbf4408a0374acaa3d
ZhakupovToday <Zhakupov.work@mail.ru>
2022-07-10 16:30:08 +0300

    Урок №3 - Экземпляры класса

--- lib/steps/DartBase.dart
+++ lib/steps/DartBase.dart
@@ -1,9 +1,12 @@
+import 'package:ft_md_101_dart/steps/ClassInstance.dart';
+
 class DartBase {
   void run(){
     print('DartBase: Hello World');
 
     _stepConf();
     _stepDataType();
+    _stepClassInstance();
   }
 
   void _stepConf(){
@@ -25,5 +28,11 @@ class DartBase {
 
     DartBase db = DartBase();
     db._stepConf();
+  }
+
+
+  void _stepClassInstance(){
+    ClassInstance ci = ClassInstance();
+    ci.run();
   }
 }
