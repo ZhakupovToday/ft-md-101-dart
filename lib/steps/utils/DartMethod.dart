3ba45e0248adaeac1b4d8dd7b2b9823d474e30f5
ZhakupovToday <Zhakupov.work@mail.ru>
2022-07-10 17:00:28 +0300

    Урок №5 - Методы

---
+++ lib/steps/utils/DartMethod.dart
@@ -0,0 +1,15 @@
+import 'dart:math';
+
+class DartMethod{
+  void run(){
+  _print();
+  }
+  void _print(){
+    print('DartMethod: ' + _generateMethodName());
+  }
+
+  String _generateMethodName(){
+    var random = Random();
+    return random.nextDouble().toString();
+  }
+}
